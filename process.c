c5cf9f3845a9e899683ec87de49be4567a65c04f
Your Name <you@example.com>
2019-04-30 23:43:32 +0900

    create process

---
+++ process.c
@@ -0,0 +1,19 @@
+/*
+ * process.c
+ *
+ *  Created on: 2019/04/30
+ *      Author: wataru65
+ */
+
+#include "lpc13xx.h"
+#include "gpio.h"
+#include "vs-wrc103.h"
+#include "ixbus.h"
+#include <stdio.h>
+
+#include "basicFunctions.h"
+#include "process.h"
+
+void process(int inputSensor){
+
+}
---
+++ process.h
@@ -0,0 +1,14 @@
+/*
+ * process.h
+ *
+ *  Created on: 2019/04/30
+ *      Author: wataru65
+ */
+
+#ifndef PROCESS_H_
+#define PROCESS_H_
+
+void process(int inputSensor);
+
+
+#endif /* PROCESS_H_ */
