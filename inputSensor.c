6feb7094e424ddcb9b052d84f4d55a5a1c18a30b
Your Name <you@example.com>
2019-04-30 23:52:07 +0900

    create inputSensor.c inputSensor.h

---
+++ inputSensor.c
@@ -0,0 +1,19 @@
+/*
+ * inputSensor.c
+ *
+ *  Created on: 2019/04/30
+ *      Author: wataru65
+ */
+#include "lpc13xx.h"
+#include "gpio.h"
+#include "vs-wrc103.h"
+#include "ixbus.h"
+
+#include "basicFunctions.h"
+#include "inputSensor.h"
+
+
+int inputSensor(){
+
+	return 0;
+}
---
+++ inputSensor.h
@@ -0,0 +1,14 @@
+/*
+ * inputSensor.h
+ *
+ *  Created on: 2019/04/30
+ *      Author: wataru65
+ */
+
+#ifndef INPUTSENSOR_H_
+#define INPUTSENSOR_H_
+
+int inputSensor();
+
+
+#endif /* INPUTSENSOR_H_ */
